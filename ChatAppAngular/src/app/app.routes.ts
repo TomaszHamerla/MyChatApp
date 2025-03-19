1c16960ef43ccf7500cd83ae0418a83d0c6c36c0
unknown <tomaszhamerla@interia.pl>
2025-03-19 17:51:30 +0100

    added password-reset, stylizing

--- ChatAppAngular/src/app/app.routes.ts
+++ ChatAppAngular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {ResetPasswordComponent} from "./views/reset-password/reset-password.component";
+import {LoginComponent} from "./views/login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'reset-password', component: ResetPasswordComponent}
+];
