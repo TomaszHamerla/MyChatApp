1d11f01d25549525cb769d60413c187848753f59
unknown <tomaszhamerla@interia.pl>
2025-04-10 18:12:35 +0200

    added chat and message entity/model

---
+++ ChatApp/src/main/java/com/example/chatapp/model/Chat.java
@@ -0,0 +1,34 @@
+package com.example.chatapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "chat")
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "recipient_id")
+    private User recipient;
+
+    @OneToMany(mappedBy = "chat", fetch = FetchType.EAGER)
+    @OrderBy("createdDate DESC")
+    private List<Message> messages;
+}
