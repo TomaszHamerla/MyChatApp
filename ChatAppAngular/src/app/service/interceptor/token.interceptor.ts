c5ecee0eff4e9427fe3d52174ef149b0bc60eee0
unknown <tomaszhamerla@interia.pl>
2025-05-08 18:04:15 +0200

    handling logout when token is expired in interceptor

--- ChatAppAngular/src/app/service/interceptor/token.interceptor.ts
+++ ChatAppAngular/src/app/service/interceptor/token.interceptor.ts
@@ -1,8 +1,11 @@
-import {HttpInterceptorFn} from '@angular/common/http';
+import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
 import {inject} from "@angular/core";
 import {TokenService} from "../utils/token.service";
+import {AuthService} from "../api/auth.service";
+import {catchError, throwError} from "rxjs";
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
   const token = inject(TokenService).token;
   if (token) {
     const cloneReq = req.clone({
@@ -10,7 +13,14 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
         Authorization: `Bearer ${token}`
       }
     })
-    return next(cloneReq);
+    return next(cloneReq).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401 || err.status === 403) {
+          authService.logout();
+        }
+        return throwError(() => err);
+      })
+    )
   }
   return next(req);
 };
